3d8e110d1360ffdafeafe882a21c0db6f497502e
preethi283 <80104315+preethi283@users.noreply.github.com>
2021-07-26 23:13:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;//1-6
-var randomdice="images/dice"+randomNumber1+".png";//dice1.png-dice6.png
+var randomdice="dice"+randomNumber1+".png";//dice1.png-dice6.png
 document.querySelectorAll("img")[0].setAttribute("src",randomdice);
 var randomNumber2=Math.floor(Math.random()*6)+1;
-var randomdice2="images/dice"+randomNumber2+".png";
+var randomdice2="dice"+randomNumber2+".png";
 document.querySelectorAll("img")[1].setAttribute("src",randomdice2);
 if(randomNumber1>randomNumber2){
     document.querySelector("h1").innerHTML="player 1 wins";
